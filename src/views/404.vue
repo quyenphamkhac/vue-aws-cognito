76986ee90912b3a681691c55ac5202185102c152
quyenphamkhac <pkquyen@sk-global.biz>
2019-03-15 11:19:19 +0700

    page 404 style

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,9 @@
 <template>
   <a-layout id="page-404">
-    <h1>404 Not found</h1>
+    <section>
+      <h1>404</h1>
+      <p>Page not found <a href="/app">Home page</a></p>
+    </section>
   </a-layout>
 </template>
 
@@ -12,10 +15,38 @@ export default {
 
 <style scoped>
 #page-404 {
-  height: auto;
-  min-height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 100;
+  width: 100%;
+  height: 100%;
+  background-image: url(https://os.alipayobjects.com/rmsportal/NOAjOBbnYCrNzrW.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+  background-attachment: fixed;
+}
+
+#page-404 h1 {
+  color: #1890ff;
+  font-weight: 500;
+  font-size: 120px;
+  margin: 0;
+}
+
+#page-404 p {
+  font-weight: 700;
+  font-size: 20px;
+}
+
+#page-404 section {
+  position: absolute;
+  top: 48%;
+  left: 55%;
+  margin: -103px 0 0 -120px;
+  text-align: center;
 }
 </style>
